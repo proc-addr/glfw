39679076214a1768dfd21e56c15a3ede17ed86cb
Camilla Berglund <elmindreda@elmindreda.org>
2012-12-03 14:15:23 +0100

    Formatted comment tag.

--- src/win32_platform.h
+++ src/win32_platform.h
@@ -68,7 +68,7 @@
 
 
 //========================================================================
-// Hack: Define things that some windows.h variants don't
+// HACK: Define things that some windows.h variants don't
 //========================================================================
 
 #ifndef WM_MOUSEHWHEEL
